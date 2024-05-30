d6cbf14fbc5eb9eeee1965c70c0b0dfae728e175
5353@laksamanacollege.edu.bn <saidir6g@gmail.com>
2024-05-31 02:33:39 +0800

    update 1

--- KBAGA/Areas/Identity/Data/ApplicationDbContext.cs
+++ KBAGA/Areas/Identity/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace KBAGA.Areas.Identity.Data;
 
@@ -9,7 +10,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
-    {
+    {        
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
@@ -18,5 +19,15 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
     }
 }
