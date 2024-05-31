04f1437978c248a903dc0d494ac597cc7131a324
5353@laksamanacollege.edu.bn <saidir6g@gmail.com>
2024-05-31 09:42:06 +0800

    FINAL UPDATE

--- KBAGA/Pages/Checkout.cshtml.cs
+++ KBAGA/Pages/Checkout.cshtml.cs
@@ -1,12 +1,24 @@
+using KBAGA.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace KBAGA.Pages
 {
     public class CheckoutModel : PageModel
     {
-        public void OnGet()
+        private readonly KBAGA.Areas.Identity.Data.ApplicationDbContext _context;
+
+        public CheckoutModel(KBAGA.Areas.Identity.Data.ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public IList<Menu> Menu { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Menu = await _context.Menu.ToListAsync();
         }
     }
 }
--- KBAGA/wwwroot/js/site.js
+++ KBAGA/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// checkout.js
+
+function showPaymentMessage(itemName) {
+    // Display the payment message in a pop-up
+    alert("Payment Successful! Thank you for your purchase.");
